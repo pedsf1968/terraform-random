e4bdbec7eca091dc72dd65f0b46b2f67a4e24412
terraform <terraform@Ubuntu-DSK.hawkfund.kr>
2022-09-12 19:03:00 +0200

    after variables change

--- random_pet_with_variables/variables.tf
+++ random_pet_with_variables/variables.tf
@@ -1,9 +1,11 @@
 variable "filename" {
-    default = "/home/terraform/data/random/random_pet_with_variables/pets.txt"
+#    default = "/home/terraform/data/random/random_pet_with_variables/pets.txt"
+    default = "/home/terraform/data/random/random_pet_with_variables/pets2.txt"
 }
 
 variable "content" {
-    default = "We love pets!"
+#    default = "We love pets!"
+    default = "My favorite pet is Mrs. Whiskers"
 }
 
 variable "prefix" {
@@ -15,5 +17,6 @@ variable "separator" {
 }
 
 variable "length" {
-    default = "1"
+#    default = "1"
+    default = "2"
 }
