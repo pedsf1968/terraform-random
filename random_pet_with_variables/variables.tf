f1fe9ec9d1d164b8fd80dc86c0f8c8b22e6f2aef
terraform <terraform@Ubuntu-DSK.hawkfund.kr>
2022-09-12 18:51:49 +0200

    multiple providers with variables in file

---
+++ random_pet_with_variables/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "/home/terraform/data/random/random_pet_with_variables/pets.txt"
+}
+
+variable "content" {
+    default = "We love pets!"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}
