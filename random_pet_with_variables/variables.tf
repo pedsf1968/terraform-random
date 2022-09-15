76f2da6e55fa44da179cb13e14610184dd2e07ca
terraform <terraform@isa-server>
2022-09-15 18:24:59 +0200

    resource with variables in separate file

--- random_pet_with_variables/variables.tf
+++ random_pet_with_variables/variables.tf
@@ -1,19 +1,26 @@
 variable "filename" {
 #    default = "/home/terraform/data/random/random_pet_with_variables/pets.txt"
-    default = "/home/terraform/data/random/random_pet_with_variables/pets2.txt"
+   default = "/home/terraform/data/random/random_pet_with_variables/pets2.txt"
+	type = string
+	description = "the path of local file"
 }
 
 variable "content" {
 #    default = "We love pets!"
-    default = "My favorite pet is Mrs. Whiskers"
+   default = "My favorite pet is Mrs. Whiskers"
+	type = string
+	description = "the content of the file"
 }
 
 variable "prefix" {
-    default = "Mrs"
+   default = "Mrs"
+	type = string
+	description = "The prefixe to use for the pet"
 }
 
 variable "separator" {
-    default = "."
+   default = "."
+	type = string
 }
 
 variable "length" {
---
+++ random_pet_with_variables_noargs/variables.tf
@@ -0,0 +1,14 @@
+variable "filename" {
+}
+
+variable "content" {
+}
+
+variable "prefix" {
+}
+
+variable "separator" {
+}
+
+variable "length" {
+}
