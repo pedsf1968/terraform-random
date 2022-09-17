f28a4bb5d9e9064319372d204b4534df7bc2e6bb
terraform <terraform@isa-server>
2022-09-17 14:06:33 +0200

    Example with random string and local_file

---
+++ random_string/variables.tf
@@ -0,0 +1,13 @@
+variable "length" {
+    default = 10
+  
+}
+variable "filename" {
+    default = "/home/terraform/data/random/random_string/random_text"
+}
+variable "content" {
+    default = "This file contains a single line of data"
+}
+variable "permission" {
+    default = 0700
+}
